22db292020b6a15f0a9de919c1886bb1f0e1def0
W. Andrew Loe III <andrew@andrewloe.com>
2009-08-22 01:51:36 -0700

    Log these actions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,11 @@ class ApplicationController < ActionController::Base
   protected
   
   def authenticate_user
-    @current_user = User.find_by_id(session[:user_id])
+    current_user
   end
   
   def current_user
-    @current_user
+    @current_user ||= User.find_by_id(session[:user_id])
   end
   helper_method :current_user
 end
