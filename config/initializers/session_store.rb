97a8580d18b48e7e14ead479f361561634da0f15
Leigh Caplan <lcaplan@onehub.com>
2009-08-21 17:00:40 -0700

    Initial commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key    => "_CHANGEME_session",
-  :secret => "CHANGESESSION"
+  :key    => "_quippo_session",
+  :secret => "8af5bd03abe59bf2c688e79252218164"
 }
 
 # Use the database for sessions instead of the cookie-based default,
