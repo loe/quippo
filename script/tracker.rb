39654dceb33b19c0d86e8a67c8046ccb00daf30d
Leigh Caplan <lcaplan@onehub.com>
2009-08-22 09:20:47 -0700

    I should really make sure constants actually exist before throwing them into my code.

--- script/tracker.rb
+++ script/tracker.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../config/environment'
 begin
   puts "=> Tracking: #{Quippo.config.twitter[:track].join(", ")}"
   TweetTracker.new.track(Quippo.config.twitter[:track])
-rescue Yajl::ParserError => e
+rescue Yajl::ParseError => e
   puts "Encountered a Yajl::ParserError: #{e.message}"
   retry
 end
