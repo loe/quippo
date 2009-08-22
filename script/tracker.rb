09cfa16eb4bb66afe86d530a81545e80edc0f966
Leigh Caplan <lcaplan@onehub.com>
2009-08-22 08:44:12 -0700

    Whoops, I had misnamed the Yajl class.

--- script/tracker.rb
+++ script/tracker.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../config/environment'
 begin
   puts "=> Tracking: #{Quippo.config.twitter[:track].join(", ")}"
   TweetTracker.new.track(Quippo.config.twitter[:track])
-rescue YAJL::ParserError => e
-  puts "Encountered a YAJL::ParserError: #{e.message}"
+rescue Yajl::ParserError => e
+  puts "Encountered a Yajl::ParserError: #{e.message}"
   retry
 end
