3d1c87f8d0122f95096f864d00613eec04b0dc51
Leigh Caplan <lcaplan@onehub.com>
2009-08-23 09:45:42 -0700

    Starting the Advanced Attribution Engine™

--- test/unit/quip_test.rb
+++ test/unit/quip_test.rb
@@ -3,4 +3,8 @@ require 'test_helper'
 class QuipTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   should_validate_presence_of :text
+  
+  context "#filter_attributions!" do
+    
+  end
 end
