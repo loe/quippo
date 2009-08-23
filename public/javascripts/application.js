23a9ebb01825b15467d3bf6fee4df47219154932
Leigh Caplan <lcaplan@onehub.com>
2009-08-23 07:50:51 -0700

    apparently document.observe only works with inline javascript?

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,9 @@
 // Move all flashes offscreen after 4 seconds
-document.observe('dom:loaded', function() {
-  flash = $('flash');
-  sky   = $('sky');
-  
-  move_offset = flash.getHeight();
-  
-  new Effect.Move(sky, {mode: 'relative', x: 0, y: -move_offset, duration: 1.3});
-}.delay(4));
+// document.observe('dom:loaded', function() {
+//   flash = $('flash');
+//   sky   = $('sky');
+//   
+//   move_offset = flash.getHeight();
+//   
+//   new Effect.Move(sky, {mode: 'relative', x: 0, y: -move_offset, duration: 1.3});
+// }.delay(4));
