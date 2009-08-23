d81ee5d181a3e5f775305b87d9d008f0e6017a1f
Leigh Caplan <lcaplan@onehub.com>
2009-08-22 19:04:32 -0700

    More crimes against humanity.

--- app/controllers/quips_controller.rb
+++ app/controllers/quips_controller.rb
@@ -1,12 +1,8 @@
 class QuipsController < ApplicationController
-  before_filter :get_user
+  before_filter :get_user, :prepare_search
   
   def index
-    search_hash = {}
-    search_hash[:text_search] = params[:q].split(/\s|,/) if params[:q]
-    logger.debug search_hash.inspect
-    
-    @quips = (@user.try(:quips) || Quip).descending.search(search_hash).paginate
+    @quips = (@user.try(:quips) || Quip).descending.search(@search_hash).paginate
     
     respond_to do |wants|
       wants.html {  }
@@ -16,9 +12,9 @@ class QuipsController < ApplicationController
   
   def show    
     if params[:id] == 'random'
-      @quips = (@user.try(:quips) || Quip).random.find(:all, :limit => 1, :include => :user)
+      @quips = [(@user.try(:quips) || Quip).random.search(@search_hash).first]
     else
-      @quips = (@user.try(:quips) || Quip).find_all_by_id(params[:id])
+      @quips = (@user.try(:quips) || Quip).search(@search_hash).find_all_by_id(params[:id])
     end
     
     respond_to do |wants|
@@ -33,5 +29,10 @@ class QuipsController < ApplicationController
     if params[:user_id]
       redirect_to(root_url) and return false unless @user = User.find_by_twitter_screen_name(params[:user_id])
     end
+  end
+  
+  def prepare_search
+    @search_hash = {}
+    @search_hash[:text_search] = params[:q].split(/\s|,/) if params[:q]
   end
 end
