6a71e44fb504e472ea047660666f3e8dbbea845d
W. Andrew Loe III <andrew@andrewloe.com>
2009-08-22 18:35:26 -0700

    Extra end.

--- app/controllers/quips_controller.rb
+++ app/controllers/quips_controller.rb
@@ -1,35 +1,34 @@
 class QuipsController < ApplicationController
   before_filter :get_user
-  
+
   def index
-    @quips = (@user.try(:quips) || Quip).descending.paginate(:include => :user, :page => params[:page], :order => 'id DESC')
-    
+    @quips = (@user.try(:quips) || Quip).descending.paginate(:include => :user, :page => params[:page])
+
     respond_to do |wants|
       wants.html {  }
-			wants.js {  }
+      wants.js {  }
       wants.json { render :json => @quips.to_json }
     end
   end
-  
+
   def show
     if params[:id] == 'random'
       @quips = (@user.try(:quips) || Quip).random.first
     else
       @quips = (@user.try(:quips) || Quip).find(params[:id])
     end
-    
+
     respond_to do |wants|
       wants.html { render :action => 'index' }
       wants.json { render :json => @quips.to_json }
     end
-    end
   end
-  
+
   protected
-  
+
   def get_user
     if params[:user_id]
-      redirect_to(root_url) and return false unless @user = User.find_by_twitter_screen_name(params[:user_id])
+      redirect_to(root_url) unless @user = User.find_by_twitter_screen_name(params[:user_id])
     end
   end
 end
