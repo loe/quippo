c5642d896033fe20157714272c215902036546e1
Leigh Caplan <lcaplan@onehub.com>
2009-08-23 10:24:38 -0700

    Performed some regex massage

--- test/unit/sanitizer_test.rb
+++ test/unit/sanitizer_test.rb
@@ -4,12 +4,12 @@ class SanitizerTest < ActiveSupport::TestCase
   context ".sanitize_terms" do
     setup do
       @terms    = %w(qp quippo)
-      @string   = "@quippo @other I have a qp awesome quip for you #other #qp #other2 #quippo"
+      @string   = "@quippo @other I have a qp awesome quip for you @quippopotamus #other #qp #other2 #quippo"
     end
 
     should "remove hashtags and @replies for search terms only" do
       output = Quippo::Sanitizer.sanitize_terms(@string, *@terms)
-      assert_equal output, "@other I have a qp awesome quip for you #other #other2"
+      assert_equal output, "@other I have a qp awesome quip for you @quippopotamus #other #other2"
     end
   end
   
