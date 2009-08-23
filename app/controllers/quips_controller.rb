26693c3fe75ba2f0b1478fc5f188dc13f69c592b
Leigh Caplan <lcaplan@onehub.com>
2009-08-23 16:01:42 -0700

    Fucking thing sucks. WE'LL DO IT LIVE!

--- app/controllers/quips_controller.rb
+++ app/controllers/quips_controller.rb
@@ -16,7 +16,7 @@ class QuipsController < ApplicationController
     if params[:id] == 'random'
       @quips = (@user.try(:quips) || Quip).random.search(@search_hash.merge(:include => :user, :limit => 1, :page => nil))
     else
-      @quips = (@user.try(:quips) || Quip).search(:id => params[:id], :page => nil)
+      @quips = (@user.try(:quips) || Quip).find_all_by_id(params[:id])
     end
     
     respond_to do |wants|
