47ff35499c04c6459b591439c9dc482c5d3b2d5b
Leigh Caplan <lcaplan@onehub.com>
2009-08-22 19:25:21 -0700

    Ghetto search: ACTIVATE!

--- app/controllers/quips_controller.rb
+++ app/controllers/quips_controller.rb
@@ -1,11 +1,8 @@
 class QuipsController < ApplicationController
-  before_filter :get_user
+  before_filter :get_user, :prepare_search
   
   def index
-    search_hash = {}
-    search_hash[:text_search] = params[:q].split(/\s|,/) if params[:q]
-    
-    @quips = (@user.try(:quips) || Quip).descending.search(search_hash.merge(:include => :user, :page => params[:page]))
+    @quips = (@user.try(:quips) || Quip).descending.search(@search_hash.merge(:include => :user, :page => params[:page]))
     
     respond_to do |wants|
       wants.html {  }
@@ -16,9 +13,9 @@ class QuipsController < ApplicationController
   
   def show
     if params[:id] == 'random'
-      @quips = (@user.try(:quips) || Quip).random.paginate(:all, :page => nil, :per_page => 1, :include => :user)
+      @quips = (@user.try(:quips) || Quip).search_for(@search_hash).random.paginate(:all, :page => nil, :per_page => 1, :include => :user)
     else
-      @quips = (@user.try(:quips) || Quip).paginate(:all, :page => nil, :per_page => 1, :conditions => {:id => params[:id]})
+      @quips = (@user.try(:quips) || Quip).search_for(@search_hash).paginate(:all, :page => nil, :per_page => 1, :conditions => {:id => params[:id]})
     end
     
     respond_to do |wants|
@@ -33,5 +30,10 @@ class QuipsController < ApplicationController
     if params[:user_id]
       redirect_to(root_url) unless @user = User.find_by_twitter_screen_name(params[:user_id])
     end
+  end
+  
+  def prepare_search
+    @search_hash = {}
+    @search_hash[:text_search] = params[:q].split(/\s|,/) if params[:q]
   end
 end
