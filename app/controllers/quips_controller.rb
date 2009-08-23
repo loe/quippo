4f6cd90c0ac0a44aa45b106f3bf5e48c7e2e5227
Leigh Caplan <lcaplan@onehub.com>
2009-08-22 18:28:59 -0700

    This JV implementation of search probably constitutes a crime against humanity.

--- app/controllers/quips_controller.rb
+++ app/controllers/quips_controller.rb
@@ -2,7 +2,11 @@ class QuipsController < ApplicationController
   before_filter :get_user
   
   def index
-    @quips = (@user.try(:quips) || Quip).descending.all.paginate
+    search_hash = {}
+    search_hash[:text_search] = params[:q].split(/\s|,/) if params[:q]
+    logger.debug search_hash.inspect
+    
+    @quips = (@user.try(:quips) || Quip).descending.search(search_hash).paginate
     
     respond_to do |wants|
       wants.html {  }
@@ -10,21 +14,17 @@ class QuipsController < ApplicationController
     end
   end
   
-  def show
+  def show    
     if params[:id] == 'random'
-      @quips = (@user.try(:quips) || Quip).random.find(:all, :limit => 1)
+      @quips = (@user.try(:quips) || Quip).random.find(:all, :limit => 1, :include => :user)
     else
-      @quips = (@user.try(:quips) || Quip).find(params[:id])
+      @quips = (@user.try(:quips) || Quip).find_all_by_id(params[:id])
     end
     
     respond_to do |wants|
       wants.html { render :action => 'index' }
       wants.json { render :json => @quips.to_json }
     end
-  end
-  
-  def method_name
-    
   end
   
   protected
