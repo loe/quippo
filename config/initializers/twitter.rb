b46bdad7f1794221e6a339888b0873bea09a31bf
W. Andrew Loe III <andrew@andrewloe.com>
2010-06-02 20:04:24 -0700

    We're no longer mining the twitter API.

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -3,5 +3,4 @@ module Quippo
   
   self.config = OpenStruct.new unless self.config
   self.config.twitter = HashWithIndifferentAccess.new(YAML.load(File.read("#{RAILS_ROOT}/config/twitter.yml"))[RAILS_ENV])
-  self.config.twitter.merge!(YAML.load(File.read("#{RAILS_ROOT}/config/twitter_api.yml")))
 end
